6322eef4ca7fe56ecf0927a9a700deac45f02f4d
sprngcoffer <1959468554@qq.com>
2025-08-17 13:36:37 +0800

    新增antd组件接入

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/reset.css";
 
 import App from "./App.vue";
 import router from "./router";
@@ -10,5 +12,6 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(Antd);
 
 app.mount("#app");
